db3a4abc86972233a8687c8d38cd5286d3e31a5d
j0rdanaaa <j0rdanaaa@gmail.com>
2023-08-19 08:59:22 -0300

    Custom Hooks Template

---
+++ src/hooks/useCapturarNome.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../constants/constants";
+
+const useCapturarNome = () => {
+  const [nomeUsuarios, setNomeUsuarios] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}users`)
+      .then((response) => {
+        setNomeUsuarios(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return nomeUsuarios
+  
+};
+
+export default useCapturarNome;
--- src/pages/CommentsPage.js
+++ src/pages/CommentsPage.js
@@ -1,46 +1,38 @@
 import { useState, useEffect } from "react";
 import { BASE_URL } from "../constants/constants";
 import axios from "axios";
-import {Title,PostContainer } from '../style'
-import { Card } from '../components/Card/Card'
+import { Title, PostContainer } from "../style";
+import { Card } from "../components/Card/Card";
+import useCapturarPostagem from "../hooks/UseCapturarPostagem";
+import useRequestData from "../hooks/useRequestData";
 
-
-const  CommentsPage = () => {
-  const [postagens, setPostagens] = useState([]);
-
-
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}comments`)
-      .then((response) => {
-        setPostagens(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+const CommentsPage = () => {
+  const [postagens, isLoading, isError] = useRequestData("comments");
 
   return (
     <div>
       <Title>Comentários dos usuários</Title>
       <PostContainer>
-
-      {postagens.map((post) => {
-        //console.log(post);
-        return(
-          <Card 
-          key={post.id} 
-          text={post.body} 
-          backgroudColor={'#1dc690'}
-          textColor={'#ffffff'}
-          />)
-      })}
+        {isError ? (
+          <p>Erro na Postagem! Tente Novamente...</p>
+        ) : isLoading ? (
+          <p>CARREGANDO...</p>
+        ) : (
+          postagens.map((post) => {
+            //console.log(post);
+            return (
+              <Card
+                key={post.id}
+                text={post.body}
+                backgroudColor={"#1dc690"}
+                textColor={"#ffffff"}
+              />
+            );
+          })
+        )}
       </PostContainer>
     </div>
   );
-}
+};
 
 export default CommentsPage;
-
-
-
